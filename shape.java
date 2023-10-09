b670e898db5459fe458caddaa8cf1ee4fecb7470
R Thananjayan <145592087+thananjayan9@users.noreply.github.com>
2023-10-09 17:51:28 +0530

    Add files via upload

---
+++ shape.java
@@ -0,0 +1,193 @@
+
+
+public abstract class Shape {
+    protected String color;
+    protected boolean filled;
+    public Shape() {
+        color = "unknown";
+        filled = false;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    @Override
+    public String toString() {
+        return "Shape[color=" + color + ", filled=" + filled + "]";
+    }
+}
+
+
+class Circle extends Shape{
+    protected double radius;
+
+    public Circle() {
+        super();
+        radius = 1.0;
+    }
+    public Circle(double radius) {
+        super();
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" + super.toString() + ", radius=" + radius + "]";
+    }
+}
+
+class Rectangle extends Shape {
+    protected double width;
+    protected double length;
+
+    public Rectangle() {
+        this.width = 1.0;
+        this.length = 1.0;
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[" + super.toString() + ", width=" + width + ", length=" + length + "]";
+    }
+}
+
+class Square extends Rectangle {
+    public Square() {
+        super();
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return width;
+    }
+
+    public void setSide(double side) {
+        setWidth(side);
+        setLength(side);
+    }
+
+    @Override
+    public void setWidth(double side) {
+        super.setWidth(side);
+        super.setLength(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        super.setWidth(side);
+        super.setLength(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square[" + super.toString() + "]";
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0, "red", true);
+        System.out.println(circle);
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Perimeter: " + circle.getPerimeter());
+
+        Rectangle rectangle = new Rectangle(3.0, 4.0, "blue", false);
+        System.out.println(rectangle);
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Perimeter: " + rectangle.getPerimeter());
+
+        Square square = new Square(2.5, "green", true);
+        System.out.println(square);
+        System.out.println("Area: " + square.getArea());
+        System.out.println("Perimeter: " + square.getPerimeter());
+    }
+}
